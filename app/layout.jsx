080a63872e81da6ae1ffb170f3ae07758b5bccbd
kevinanielsen <keveran@gmail.com>
2023-01-26 13:40:50 +0100

    added account pop up

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,14 @@
 'use client';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
 import { CheckSquare, Moon, Sun, User } from "phosphor-react";
 import { useEffect, useState } from 'react';
 import './globals.css';
 
 export default function RootLayout({ children }) {
   const [theme, setTheme] = useState('light');
+  const [shown, setShown] = useState(false);
+  const [signedIn, setSignedIn] = useState(false);
 
   useEffect(() => {
     if(theme === "dark") {
@@ -18,8 +22,32 @@ export default function RootLayout({ children }) {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   }
 
+  const signIn = () => {
+    setSignedIn(signedIn ? false : true);
+  }
+
+  const showMenu = {
+    enter: {
+      opacity: 1,
+      y: 0,
+      display: "block",
+      x: -105,
+    },
+    exit: {
+      y: -5,
+      x: -105,
+      opacity: 0,
+      transition: {
+        duration: 0.3,
+      },
+      transitionEnd: {
+        display: "none",
+      },
+    },
+  };
+
   return (
-    <html lang="en" className='dark' >
+    <html lang="en" className="" >
       <head />
       <body className='bg-white dark:bg-bkg'>
         <header>
@@ -35,8 +63,43 @@ export default function RootLayout({ children }) {
                 </a> 
               </div>
                 <div className="flex gap-2">
+                  <motion.div
+                    onHoverStart={() => setShown(true)}
+                    onHoverEnd={() => setShown(false)}
+                  >
+                    <User size={32} aria-label="Account" />
+                    <motion.ul
+                      variants={showMenu}
+                      initial="exit"
+                      animate={shown ? "enter" : "exit"}
+                      className="absolute bg-text dark:bg-darker rounded-lg p-6"
+                    >
+                      <Link href="/">
+                        <motion.li
+                          whileHover={{
+                            color: "#00FFC2",
+                            y: -2,
+                          }}
+                          className="cursor-pointer p-1 px-3 py-2 bg-white text-bkg dark:bg-bkg dark:text-text rounded-lg mb-2.5 w-36 flex items-center justify-center"
+                        >
+                          Account
+                        </motion.li>
+                      </Link>
+                      <button onClick={signIn}>
+                        <motion.li
+                          whileHover={{
+                            color: "#00FFC2",
+                            y: -2,
+                          }}
+                          className="cursor-pointer p-1 px-3 py-2 bg-white text-bkg dark:bg-bkg dark:text-text rounded-lg w-36 flex items-center justify-center"
+                        >
+                          {signedIn ? "Sign out" : "Sign in"}
+                        </motion.li>
+                      </button>
+                    </motion.ul>
+                  </motion.div>
+
                   {theme === 'light' ? <Moon size={32} onClick={handleThemeSwitch} className="cursor-pointer" aria-label="Switch to dark theme" /> : <Sun size={32} onClick={handleThemeSwitch} className="cursor-pointer" aria-label="Switch to light theme" /> }
-                  <User size={32} aria-label="Account" />
                 </div>
             </div>           
           </nav>
