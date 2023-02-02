b9a6f77d7cd97857cb0995873fc41f976ec064ea
Kevin Nielsen <keveran@gmail.com>
2023-02-02 13:36:10 +0100

    fixed build bug

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,4 @@
-'use client';
-import { useRouter } from 'next/navigation';
-
 export default function Home() {
-  const router = useRouter();
-  router.push('/account');
-
   return (
     <main>
       Go to <a href="./todos">Todos</a>
