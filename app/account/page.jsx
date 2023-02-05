7e0b4c5bab71cbbaf7e15446592007589b9ada7d
kevinanielsen <keveran@gmail.com>
2023-02-05 17:37:06 +0100

    fixed build issue

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -2,20 +2,21 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { getUserNameClient } from '../util/getUserClient';
-// import { db } from '../util/pocketbase';
 
 export default function Account() {
   const router = useRouter();
   const user = getUserNameClient();
 
   if(!user){
-    router.push('/login');
+    setTimeout(()=> {
+      router.push('/login')
+    }, 500)
   }
 
   return(
     <main>
       <h1>Work in progress</h1>
-      <h2>Hello, {user}</h2>
+      {user && <h1>Welcome, {user}</h1>}
       <Link href="/todos">Go to your todos</Link>
     </main>
   )
